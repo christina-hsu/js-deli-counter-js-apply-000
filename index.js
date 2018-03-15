ac6e80aa46071fd9c3e8158baf484c80980161d0
Christina HSu <christinahsu.th@gmail.com>
2018-03-15 19:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line){
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length == 0){
+  if(katzDeliLine.length== 0){
     return "The line is currently empty."
   } else {
     var str = "The line is currently: " + "1. " + katzDeliLine[0]
